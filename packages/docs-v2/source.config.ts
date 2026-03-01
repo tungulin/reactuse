fd9d9cf7e48081dac1df91a2fcc91d2a5f221969
tungulin <tungulin2002@gmail.com>
2026-03-01 16:53:01 +0300

    feature/new-docs 🧊 init docs

---
+++ packages/docs-v2/source.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig, defineDocs } from "fumadocs-mdx/config"
+import rehypePrettyCode from "rehype-pretty-code"
+
+import { transformers } from "@/lib/highlight-code"
+
+export default defineConfig({
+  mdxOptions: {
+    rehypePlugins: (plugins) => {
+      plugins.shift()
+      plugins.push([
+        rehypePrettyCode,
+        {
+          theme: {
+            dark: "github-dark",
+            light: "github-light-default",
+          },
+          transformers,
+        },
+      ])
+
+      return plugins
+    },
+  },
+})
+
+export const docs = defineDocs({
+  dir: "content/docs",
+})
